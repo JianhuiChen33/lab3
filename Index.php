64698613dec0bc8a2f01cede2989a746739ccb48
MI <MI@DESKTOP-K8ULV66>
2020-09-22 15:01:47 +0800

    Added an index page

---
+++ Index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Student Index</title>
+    </head>
+    <body>
+       <?php
+
+include('Student.php'); 
+$students = array();
+$first = new Student();
+
+$first->surname = "Doe";
+
+$first->first_name = "John";
+
+$first->add_email('home','john@doe.com');
+
+$first->add_email('work','jdoe@mcdonalds.com');
+
+$first->add_grade(65);
+
+$first->add_grade(75);
+
+$first->add_grade(55);
+
+$students['j123'] = $first;
+$second = new Student();
+
+$second->surname = "Einstein";
+
+$second->first_name = "Albert";
+
+$second->add_email('home','albert@braniacs.com');
+
+$second->add_email('work1','a_einstein@bcit.ca');
+
+$second->add_email('work2','albert@physics.mit.edu');
+
+$second->add_grade(95);
+
+$second->add_grade(80);
+
+$second->add_grade(50);
+
+$students['a456'] = $second;
+$third = new Student();
+
+$third->surname = "Jianhui";
+
+$third->first_name = "Chen";
+
+$third->add_email('home','xxx@xx.com');
+
+$third->add_email('work1','xxx@xx.com');
+
+$third->add_email('work2','xxx@xx.com');
+
+$third->add_grade(95);
+
+$third->add_grade(100);
+
+$third->add_grade(70);
+
+$students['a01159926'] = $third;
+foreach($students as $student) 
+    echo $student->toString();
+
+?>
+    </body>
+</html>
